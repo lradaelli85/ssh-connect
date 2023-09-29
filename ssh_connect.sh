56452ee8724ba14189b7aa8b32aaa58af25b2012
Luca Radaelli <lradaelli85@users.noreply.github.com>
2023-09-29 10:55:46 +0200

    Add possibility to set ssh key and add pagination

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -7,6 +7,10 @@ param="$1"
 validate_ip () {
 local ip
 not_valid=0
+
+#Strip the "." and check if chars are all numbers
+if [[ ${1//.} =~ ^[[:digit:]]+$ ]]
+then
 ip=($(echo $1 |awk -F'.' '{print $1" "$2" "$3" "$4}'))
 if [ ${#ip[@]} -eq 4 ]
     then
@@ -24,6 +28,7 @@ if [ ${not_valid} -eq 1 ]
     then
         echo "not a valid ip"
         exit 1;
+fi
 fi
 }
 
@@ -43,11 +48,25 @@ fi
 }
 
 print_menu() {
+#clear
+local menu_file="none"
+if [ ${#vettore_ip[@]} -gt 20 ]
+then
+  local menu_file=$(mktemp /tmp/connect.XXXXX)
+  local echo_opt=">> \$menu_file"
+fi
+
 local cont=0
 until [ $cont -eq ${#vettore_ip[@]} ]; do
-echo $cont"_"${vettore_ip[$cont]}"->"${vettore_host[$cont]}
+eval echo "$cont\) ${vettore_ip[$cont]} --\> ${vettore_host[$cont]}" $echo_opt
 let cont=cont+1
 done
+
+if [ -f $menu_file ]
+then
+less -e -n $menu_file
+rm $menu_file
+fi
 }
 
 pop_vector() {
@@ -55,7 +74,7 @@ pop_vector() {
   err_mess="you need to adjust parameter in your $SSH_FILE"
   if [ -e $SSH_FILE ]
     then
-      while IFS="|" read ip host user port
+      while IFS="|" read ip host user port ssh_key
       do
         if [[ ! "$ip" =~ ^#|$^ ]]
         then
@@ -65,7 +84,15 @@ pop_vector() {
             vettore_host[$j]="$host"
 	        [[ ${user:?$err_mess} ]]
             vettore_user[$j]="$user"
-          vettore_port[$j]="$port"
+          [[ ${port:="22"} ]]
+            vettore_port[$j]="$port"
+          if [ -f ${ssh_key} ]
+            then
+              vettore_key[$j]="$ssh_key"
+            else
+              echo "ssh key ${ssh_key} for host \"${host}\" doesn't exists,exiting now!"
+              exit 1;
+          fi
           let j=j+1
         fi
       done < $SSH_FILE
@@ -76,19 +103,21 @@ fi
 
 ask(){
 until [[ "$id" =~ ^-?[0-9]+$ ]] && [ "$id" -ge 0 ] && [ "$id" -lt "${#vettore_ip[@]}" ]; do
-echo "choose the system ID you want to connect to"
-read id
+echo
+read -p "choose the system ID you want to connect to: " id
 done
 }
+
 
 usage
 pop_vector
 print_menu
 ask
 validate_ip ${vettore_ip[$id]}
-if [ -z ${vettore_port[$id]} ]
+
+if [ -z ${vettore_key[$id]} ]
   then
-    $SSH ${vettore_user[$id]}@${vettore_ip[$id]}
-  else
     $SSH ${vettore_user[$id]}@${vettore_ip[$id]} -p ${vettore_port[$id]}
-fi
+  else
+    $SSH -i ${vettore_key[$id]} ${vettore_user[$id]}@${vettore_ip[$id]} -p ${vettore_port[$id]}
+fi  
